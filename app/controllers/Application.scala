4a3b84907363da24b0531ebe37969f3d17e968d3
KUOKA Yusuke <ykuoka@gmail.com>
2012-12-07 17:39:49 +0900

    WebSocket-powered pomodoro

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,14 @@
 package controllers
 
 import play.api._
+import libs.iteratee._
 import play.api.mvc._
 import akka.actor._
 import akka.util.duration._
 import akka.pattern._
 import play.api.libs.concurrent._
 import akka.util.Timeout
+import java.io._
 
 case object InPomodoroNow
 
@@ -18,7 +20,7 @@ class OrenoPomodoro extends Actor {
   var inPomodoroNow = false
 
   def startPomodoro() {
-    Logger.info("Starting a pomodoro")
+    Logger.info("Starting a pomodoro: " + this.toString)
     inPomodoroNow = true
     context.system.scheduler.scheduleOnce(pomodoroDuration) {
       startBreak()
@@ -47,12 +49,12 @@ class OrenoPomodoro extends Actor {
 object Application extends Controller {
 
   val system = ActorSystem("pomodoro")
-  val orenoPomodoro = system.actorOf(Props[OrenoPomodoro])
-  
+  val globalOrenoPomodoro = system.actorOf(Props[OrenoPomodoro])
+
   def index = Action {
     Async {
       implicit val timeout = Timeout(10 seconds)
-      (orenoPomodoro ? InPomodoroNow).mapTo[Boolean].asPromise.map { inPomodoroNow =>
+      (globalOrenoPomodoro ? InPomodoroNow).mapTo[Boolean].asPromise.map { inPomodoroNow =>
         Ok(
           if (inPomodoroNow)
             "ポモドーロ中"
@@ -61,6 +63,51 @@ object Application extends Controller {
         )
       }
     }
+  }
+
+  def status(username: String) = Action { implicit request =>
+    Ok(views.html.status(username))
+  }
+
+  var actors = Map.empty[String, ActorRef]
+
+  def actorForUserName(username: String) = {
+    actors = actors.updated(username, actors.get(username).getOrElse {
+      system.actorOf(Props[OrenoPomodoro])
+    })
+    actors.get(username).get
+  }
+
+  def watch(username: String) = WebSocket.using[String] { req =>
+
+    val outputStream = new PipedOutputStream()
+    val inputStream = new PipedInputStream(outputStream)
+    val writer = new PrintWriter(outputStream)
+
+    // Start an actor
+    val orenoPomodoro = actorForUserName(username)
+
+    val out: Enumerator[String] = Enumerator.fromStream(inputStream).map(bytes => new String(bytes, "UTF-8"))
+
+    val interval = system.scheduler.schedule(0 seconds, 1 second) {
+      implicit val timeout = Timeout(10 seconds)
+      (orenoPomodoro ? InPomodoroNow).mapTo[Boolean].asPromise.map { inPomodoroNow =>
+        writer.println(
+          if (inPomodoroNow)
+            "ポモドーロ中"
+          else
+            "休憩中"
+        )
+        writer.flush()
+      }
+    }
+
+    val in = Iteratee.foreach[String](println).mapDone { _ =>
+      println("Disconnected")
+      interval.cancel()
+    }
+
+    (in, out)
   }
   
 }
